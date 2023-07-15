1fd0c664487e74060dbae1a7b8d743a6f77873e2
Keifer Wiseman <keiferwiseman@gmail.com>
2023-07-15 19:40:23 -0400

    my first typescript

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,17 @@
-function scaleContent() {
-    console.log("hello?");
-}
+var myButton = document.getElementById('myButton');
+var myOutput = document.getElementById('myOutput');
+var fontSize = 12.5; // Initial font size
+myButton.addEventListener('click', function () {
+    fontSize -= 0.5; // Increase font size by 1 pixel
+    myOutput.style.fontSize = "".concat(fontSize, "px");
+    var images = myOutput.getElementsByTagName('img');
+    for (var i = 0; i < images.length; i++) {
+        var image = images[i];
+        var currentWidth = image.width;
+        var currentHeight = image.height;
+        var newWidth = currentWidth * 0.9; // Decrease width by 10%
+        var newHeight = currentHeight * 0.9; // Decrease height by 10%
+        image.style.width = "".concat(newWidth, "px");
+        image.style.height = "".concat(newHeight, "px");
+    }
+});
--- src/main/resources/typescript/script.ts
+++ src/main/resources/typescript/script.ts
@@ -1,3 +1,18 @@
-function scaleContent() {
-  console.log("hello?");
+const myButton = document.getElementById('myButton') as HTMLButtonElement;
+const myOutput = document.getElementById('myOutput') as HTMLDivElement;
+let fontSize = 12.5; // Initial font size
+myButton.addEventListener('click', () => {
+    fontSize -= 0.5; // Increase font size by 1 pixel
+    myOutput.style.fontSize = `${fontSize}px`;
+
+const images = myOutput.getElementsByTagName('img');
+for (let i = 0; i < images.length; i++) {
+  const image = images[i] as HTMLImageElement;
+  const currentWidth = image.width;
+  const currentHeight = image.height;
+  const newWidth = currentWidth * 0.9; // Decrease width by 10%
+  const newHeight = currentHeight * 0.9; // Decrease height by 10%
+  image.style.width = `${newWidth}px`;
+  image.style.height = `${newHeight}px`;
 }
+});
