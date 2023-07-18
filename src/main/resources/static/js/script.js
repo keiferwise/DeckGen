f3ee5293bedd964b7204b5baa30601dbf10a182d
Keifer Wiseman <keiferwiseman@gmail.com>
2023-07-18 17:06:01 -0400

    first fully functional prototype?

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,5 @@
 var myButton = document.getElementById('myButton');
+var scaleUpButton = document.getElementById('scaleUpButton');
 var myOutput = document.getElementById('myOutput');
 var fontSize = 12.5; // Initial font size
 myButton.addEventListener('click', function () {
@@ -9,8 +10,22 @@ myButton.addEventListener('click', function () {
         var image = images[i];
         var currentWidth = image.width;
         var currentHeight = image.height;
-        var newWidth = currentWidth * 0.9; // Decrease width by 10%
-        var newHeight = currentHeight * 0.9; // Decrease height by 10%
+        var newWidth = currentWidth * 0.95; // Decrease width by 10%
+        var newHeight = currentHeight * 0.95; // Decrease height by 10%
+        image.style.width = "".concat(newWidth, "px");
+        image.style.height = "".concat(newHeight, "px");
+    }
+});
+scaleUpButton.addEventListener('click', function () {
+    fontSize += 0.5; // Increase font size by 1 pixel
+    myOutput.style.fontSize = "".concat(fontSize, "px");
+    var images = myOutput.getElementsByTagName('img');
+    for (var i = 0; i < images.length; i++) {
+        var image = images[i];
+        var currentWidth = image.width;
+        var currentHeight = image.height;
+        var newWidth = currentWidth * 1.05; // Decrease width by 10%
+        var newHeight = currentHeight * 1.05; // Decrease height by 10%
         image.style.width = "".concat(newWidth, "px");
         image.style.height = "".concat(newHeight, "px");
     }
--- src/main/resources/typescript/script.ts
+++ src/main/resources/typescript/script.ts
@@ -1,4 +1,6 @@
 const myButton = document.getElementById('myButton') as HTMLButtonElement;
+const scaleUpButton = document.getElementById('scaleUpButton') as HTMLButtonElement;
+
 const myOutput = document.getElementById('myOutput') as HTMLDivElement;
 let fontSize = 12.5; // Initial font size
 myButton.addEventListener('click', () => {
@@ -10,9 +12,23 @@ for (let i = 0; i < images.length; i++) {
   const image = images[i] as HTMLImageElement;
   const currentWidth = image.width;
   const currentHeight = image.height;
-  const newWidth = currentWidth * 0.9; // Decrease width by 10%
-  const newHeight = currentHeight * 0.9; // Decrease height by 10%
+  const newWidth = currentWidth * 0.95; // Decrease width by 10%
+  const newHeight = currentHeight * 0.95; // Decrease height by 10%
   image.style.width = `${newWidth}px`;
   image.style.height = `${newHeight}px`;
-}
-});
+}});
+
+scaleUpButton.addEventListener('click', () => {
+    fontSize += 0.5; // Increase font size by 1 pixel
+    myOutput.style.fontSize = `${fontSize}px`;
+
+const images = myOutput.getElementsByTagName('img');
+for (let i = 0; i < images.length; i++) {
+  const image = images[i] as HTMLImageElement;
+  const currentWidth = image.width;
+  const currentHeight = image.height;
+  const newWidth = currentWidth * 1.05; // Decrease width by 10%
+  const newHeight = currentHeight * 1.05; // Decrease height by 10%
+  image.style.width = `${newWidth}px`;
+  image.style.height = `${newHeight}px`;
+}});
