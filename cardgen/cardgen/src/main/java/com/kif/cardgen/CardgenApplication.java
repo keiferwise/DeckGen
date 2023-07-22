0622ff6dc1f7158e19d6120bd752f6da05cbd868
Keifer Wiseman <keiferwiseman@gmail.com>
2023-07-22 15:26:36 -0400

    moving project

---
+++ cardgen/cardgen/src/main/java/com/kif/cardgen/CardgenApplication.java
@@ -0,0 +1,13 @@
+package com.kif.cardgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardgenApplication.class, args);
+	}
+
+}
