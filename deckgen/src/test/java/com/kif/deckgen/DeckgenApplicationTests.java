1a50689554650e98242174265b6ce3a19e59409c
Keifer Wiseman <keiferwiseman@gmail.com>
2023-08-03 20:46:21 -0400

    added deckgen microservice

---
+++ deckgen/src/test/java/com/kif/deckgen/DeckgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kif.deckgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
