6a6c6ed1d8a6ff3e7bb65bb797e641489d1b7863
Keifer Wiseman <keiferwiseman@gmail.com>
2023-08-31 20:39:56 -0400

    card service 3

--- deckservice/src/main/java/com/kif/deckservice/controllers/DeckController.java
+++ deckservice/src/main/java/com/kif/deckservice/controllers/DeckController.java
@@ -3,7 +3,11 @@ package com.kif.deckservice.controllers;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kif.deckgenmodels.CardRequest;
+import com.kif.deckgenmodels.DeckRequest;
 
 @RestController
 public class DeckController {
@@ -12,8 +16,12 @@ public class DeckController {
 		// TODO Auto-generated constructor stub
 	}
 	@PostMapping(value = "/create-deck", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> createDeck() {
+	public ResponseEntity<String> createDeck(@RequestBody DeckRequest cr) {
+		System.out.println("creating Deck");
+		System.out.println(cr.toString());
+		// Create something to 
 		
+		// CreateCard(Card card, String theme, DeckIdea deckIdea)
 		return ResponseEntity.ok("Request received successfully!");
 	}
 }
