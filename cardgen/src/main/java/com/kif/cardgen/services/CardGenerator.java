09d9ec04c5eaf962934ae2046f426a8d0c242688
Keifer Wiseman <keiferwiseman@gmail.com>
2023-09-03 23:53:41 -0400

    card service 4

--- cardgen/src/main/java/com/kif/cardgen/services/CardGenerator.java
+++ cardgen/src/main/java/com/kif/cardgen/services/CardGenerator.java
@@ -1,5 +1,14 @@
 package com.kif.cardgen.services;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -8,50 +17,55 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kif.deckgenmodels.Card;
 import com.kif.deckgenmodels.DeckIdea;
+import com.kif.deckgenmodels.Image;
 import com.kif.cardgen.daos.CardDao;
 import com.kif.cardgen.daos.DeckIdeaDao;
+import com.kif.cardgen.daos.MinioDao;
 import com.kif.cardgen.services.*;
 
 /**
  * 
  * @author Keifer
  * 
- * TODO Class with functions that take card names and generate the cards
+ *         TODO Class with functions that take card names and generate the cards
  */
 @Component
 public class CardGenerator {
 
 	@Value("${com.kif.cardDetailsTemplate}")
 	private String cardDetailsTemplate;
-	
 
-	@Autowired 
+	@Autowired
 	ObjectMapper objectMapper;
 	@Autowired
 	ChatGPTClient gptClient;
-	@Autowired 
+	@Autowired
 	PromptBuilder pb;
 	@Autowired
 	CardDao cardDao;
 	@Autowired
 	DeckIdeaDao ideaDao;
+	@Autowired
+	CardComposer composer;
+	@Autowired
+	DalleClient dalle;
+	@Autowired
+	MinioDao minio;
 	
-
-	
-	//TODO take a card object with only a name and type and fill out the details
+	// TODO take a card object with only a name and type and fill out the details
 	public Card createCard(String cardid, String theme, String deckIdeaId) {
-		//System.out.println(cardDetailsTemplate);
-		//System.out.println("running promptbuilder");
+		// System.out.println(cardDetailsTemplate);
+		// System.out.println("running promptbuilder");
 		Card card = cardDao.getCardById(cardid);
 		DeckIdea deckIdea = ideaDao.findByDeckId(deckIdeaId);
-		
-		String prompt = pb.buildCardPrompt(card,deckIdea);
 
-		//System.out.println("the prompt is... "+prompt);
-		String newCardJson = gptClient.generateCompletion(prompt,2000);
-		//System.out.println(newCardJson);
+		String prompt = pb.buildCardPrompt(card, deckIdea);
 
-		Card newCard=null;
+		// System.out.println("the prompt is... "+prompt);
+		String newCardJson = gptClient.generateCompletion(prompt, 2000);
+		// System.out.println(newCardJson);
+
+		Card newCard = null;
 		try {
 			newCard = objectMapper.readValue(newCardJson, Card.class);
 		} catch (JsonProcessingException e) {
@@ -61,65 +75,183 @@ public class CardGenerator {
 		System.out.println(newCard.toString());
 		newCard.setCardId(card.getCardId());
 		newCard.setDeckId(card.getDeckId());
-		//System.out.println(card.getName());
-		
-		
-		//TODO Make this call the card composer to make the art.
-		
-		//TODO push this card to the Object store
-		
-		
+		// System.out.println(card.getName());
+
+		/* ### THIS decides if WE ARE MAKING ART ### */
+		String makeArt = "false";
+
+		System.out.println("Making art: " + makeArt);
+
+		BufferedImage imgTest = null;
+		BufferedImage img = null;
+		URL url = null;
+		// Generate Art for cards FINISH THIS
+		if (makeArt.equals("true")) {
+
+			// ######################################################
+			// #### What we will call when we are generating art ####
+			// ######################################################
+
+			Image art = dalle.generateImage(card.getArtDescription()).getData().get(0);
+			// get the art from Dall-E URL
+			try {
+				url = new URL(art.getUrl());
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				img = ImageIO.read(url);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} else {
+			// What we will call while we are testing ""
+			String artPath = "D:\\deckgen\\deck-gen-main\\src\\main\\resources\\images\\img-H7MTllJItxyHXRMlnO77hB9I.png";
+			try {
+				img = ImageIO.read(new File(artPath));
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+
+		}
+		// Create the card
+		BufferedImage cardImage = null;
+		try {
+			cardImage = composer.createImage(card, img);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		minio.saveImage(cardImage, card.getCardId());
+
+		// TODO Make this call the card composer to make the art.
+
+		// TODO push this card to the Object store
+
 		return newCard;
 	}
-	
+
 	private String manaColour(DeckIdea idea) {
-				
+
 		String result = " ";
-		if(idea.isBlack()) {result = result + "Black, ";};
-		if(idea.isRed()) {result = result + "Red, ";};
-		if(idea.isGreen()) {result = result + "Green, ";};
-		if(idea.isWhite()) {result = result + "White, ";};
-		if(idea.isBlue()) {result = result + "Blue, ";};
-		
-		result = result.substring(0, result.length()-2);
-		
-		
-		
+		if (idea.isBlack()) {
+			result = result + "Black, ";
+		}
+		;
+		if (idea.isRed()) {
+			result = result + "Red, ";
+		}
+		;
+		if (idea.isGreen()) {
+			result = result + "Green, ";
+		}
+		;
+		if (idea.isWhite()) {
+			result = result + "White, ";
+		}
+		;
+		if (idea.isBlue()) {
+			result = result + "Blue, ";
+		}
+		;
+
+		result = result.substring(0, result.length() - 2);
+
 		return result;
 	}
-	
+
 	private String getCardColours(Card card) {
-		String cardColour="";
-		//String path = "D:\\deckgen\\src\\main\\resources\\images\\";
-		int colourCounter=0;
-		//Get Colour Identity String
-		if(card.getManaCost().contains("W")) {cardColour+="white, "; colourCounter++;}
-		if(card.getManaCost().contains("U")) {cardColour+="blue, "; colourCounter++;}
-		if(card.getManaCost().contains("B")) {cardColour+="black, "; colourCounter++;}
-		if(card.getManaCost().contains("R")) {cardColour+="red, "; colourCounter++;}
-		if(card.getManaCost().contains("G")) {cardColour+="green, "; colourCounter++;}
-
-
-		//if(colourCounter>1) {
-		//	cardColour = "Multicolour";
-		//}
-		else if(colourCounter == 0) {
-			cardColour="Colourless";
+		String cardColour = "";
+		// String path = "D:\\deckgen\\src\\main\\resources\\images\\";
+		int colourCounter = 0;
+		// Get Colour Identity String
+		if (card.getManaCost().contains("W")) {
+			cardColour += "white, ";
+			colourCounter++;
 		}
-		//path = path + frameColour + ".png";
+		if (card.getManaCost().contains("U")) {
+			cardColour += "blue, ";
+			colourCounter++;
+		}
+		if (card.getManaCost().contains("B")) {
+			cardColour += "black, ";
+			colourCounter++;
+		}
+		if (card.getManaCost().contains("R")) {
+			cardColour += "red, ";
+			colourCounter++;
+		}
+		if (card.getManaCost().contains("G")) {
+			cardColour += "green, ";
+			colourCounter++;
+		}
 
-		//System.out.println(path);
-		return cardColour.substring(0, cardColour.length()-2);
+		// if(colourCounter>1) {
+		// cardColour = "Multicolour";
+		// }
+		else if (colourCounter == 0) {
+			cardColour = "Colourless";
+		}
+		// path = path + frameColour + ".png";
+
+		// System.out.println(path);
+		return cardColour.substring(0, cardColour.length() - 2);
 	}
 
 	public void createSingleCard() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	private String legendManaCost(DeckIdea idea) {
+		String cardManaCost = "";
+		int colourlessNumber = 0;
+		Random rand = new Random();
+		int colourCounter = 0;
 
+		if (idea.isWhite()) {
+			cardManaCost += "W";
+			colourCounter++;
+		}
+		if (idea.isBlue()) {
+			cardManaCost += "U";
+			colourCounter++;
+		}
+		if (idea.isBlack()) {
+			cardManaCost += "B";
+			colourCounter++;
+		}
+		if (idea.isRed()) {
+			cardManaCost += "R";
+			colourCounter++;
+		}
+		if (idea.isGreen()) {
+			cardManaCost += "G";
+			colourCounter++;
+		}
 
+		colourlessNumber = rand.nextInt(0, 9 - colourCounter);
 
+		if (colourlessNumber != 0) {
+			cardManaCost = colourlessNumber + cardManaCost;
+		}
 
+		return cardManaCost;
+	}
+
+	// TODO
+	private void createCardText(String cardid, String theme, String deckIdeaId) {
+
+	}
+
+	// TODO
+	private void createCardArt() {
+
+	}
 
 }
