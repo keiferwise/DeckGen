f476ba8ca9347ff82c6f22cd5060c4844d85d7c9
Keifer Wiseman <keiferwiseman@gmail.com>
2023-10-05 22:43:59 -0400

    more stuff

---
+++ deckgen-models/src/main/java/com/kif/deckgenmodels/SingleRequest.java
@@ -0,0 +1,69 @@
+package com.kif.deckgenmodels;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SingleRequest {
+	@JsonProperty("name")
+	private String name; 
+	@JsonProperty("type")
+	private String type; 
+	@JsonProperty("theme")
+	private String theme;
+	@JsonProperty("artStyle")
+	private String artStyle;
+	@JsonProperty("vibe")
+	private String vibe; 
+	@JsonProperty("mana")
+	private String mana;
+	public SingleRequest() {
+
+	
+	
+	}
+	public SingleRequest(String name, String type, String theme, String artStyle, String vibe, String mana) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.theme = theme;
+		this.artStyle = artStyle;
+		this.vibe = vibe;
+		this.mana = mana;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTheme() {
+		return theme;
+	}
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+	public String getArtStyle() {
+		return artStyle;
+	}
+	public void setArtStyle(String artStyle) {
+		this.artStyle = artStyle;
+	}
+	public String getVibe() {
+		return vibe;
+	}
+	public void setVibe(String vibe) {
+		this.vibe = vibe;
+	}
+	public String getMana() {
+		return mana;
+	}
+	public void setMana(String mana) {
+		this.mana = mana;
+	}
+
+}
