ea1cbc6cc53f53b3b094e44d2b280d8e7c07cc2e
Keifer Wiseman <keiferwiseman@gmail.com>
2023-11-14 21:20:37 -0500

    spring security

--- deck-gen-main/src/main/java/com/kif/deckgen/SecurityConfiguration.java
+++ deck-gen-main/src/main/java/com/kif/deckgen/SecurityConfiguration.java
@@ -2,11 +2,51 @@ package com.kif.deckgen;
 
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
-	public SecurityConfiguration() {
-		// TODO Auto-generated constructor stub
+	@Autowired
+	private DataSource dataSource;
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+				.authorizeHttpRequests((authorize) -> authorize
+						.anyRequest().authenticated()
+				)
+				.httpBasic(withDefaults())
+				.formLogin(withDefaults());
+		// @formatter:on
+		return http.build();
 	}
 
+	@Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+            .dataSource(dataSource)
+            .usersByUsernameQuery("select username, password, enabled from users where username=?")
+            .authoritiesByUsernameQuery("select username, role from users where username=?")
+        ;
+    }
+	
 }
--- deck-gen-main/src/main/resources/Schema/user.sql
+++ deck-gen-main/src/main/resources/Schema/user.sql
@@ -1,11 +1,11 @@
-drop table user;
+drop table users;
 
-CREATE TABLE user (
+CREATE TABLE users (
     user_id VARCHAR(36),
+    username varchar(50),
     email VARCHAR(100),
     admin TINYINT(1) DEFAULT FALSE,
-    moderator TINYINT(1) DEFAULT FALSE,
-    verified TINYINT(1) NOT NULL DEFAULT FALSE,
+	ROLE varchar(10),
     enabled TINYINT(1) NOT NULL DEFAULT TRUE,
     password VARCHAR(255),
     name VARCHAR(50),
