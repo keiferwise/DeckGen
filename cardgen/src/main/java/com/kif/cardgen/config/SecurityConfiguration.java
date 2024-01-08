fd7cc0bcc2317eb8a6efe8fb1c4d5a0052abbdab
Keifer Wiseman <keiferwiseman@gmail.com>
2024-01-07 22:57:24 -0500

    httpsecurity

--- cardgen/src/main/java/com/kif/cardgen/config/SecurityConfiguration.java
+++ cardgen/src/main/java/com/kif/cardgen/config/SecurityConfiguration.java
@@ -3,13 +3,30 @@ package com.kif.cardgen.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
+    WebSecurityCustomizer webSecurityCustomizer() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowedHostnames(hostName ->             
+        hostName.equals("localhost"));
         return (webSecurity) -> webSecurity.ignoring().requestMatchers("/create-card","/create-card-for-deck");
     }
+    /*
+    @Bean
+    StrictHttpFirewall httpFirewall() {
+      StrictHttpFirewall strictHttpFireWall = new StrictHttpFirewall();
+      strictHttpFireWall.setAllowBackSlash(true);
+      //strictHttpFireWall.setAllowedHostnames(hostName ->             
+      //hostName.equals("http://localhost"));
+      strictHttpFireWall.setAllowedHostnames(hostName ->             
+      hostName.equals("localhost"));
+      //strictHttpFireWall.setAllowedHostnames(hostName ->             
+      //hostName.equals("127.0.0.1"));
+      return strictHttpFireWall;
+    }*/
 
 }
