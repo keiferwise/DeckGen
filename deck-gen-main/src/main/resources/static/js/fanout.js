1638ba6eb95b2db3134f40bad8e7761aa5deb303
Keifer Wiseman <keiferwiseman@gmail.com>
2024-02-29 15:53:35 -0500

    Fan-out is looking good, but is not responsive

--- deck-gen-main/src/main/resources/static/js/fanout.js
+++ deck-gen-main/src/main/resources/static/js/fanout.js
@@ -1,21 +1,30 @@
 // Get all the card elements
 var cards = document.querySelectorAll('.b-card');
 // Calculate the rotation angle for each card
-var angle = 20; // Adjust this value to change the fan-out angle
+var angle = 10; // Adjust this value to change the fan-out angle
 var totalCards = cards.length;
 var initialRotation = -angle * (totalCards - 1) / 2;
 console.log("hello");
 console.log(cards.length);
 // Calculate the radius of the arch
-var radius = 200; // Adjust this value to change the radius of the arch
+var radius = 400; // Adjust this value to change the radius of the arch
+var startX = 1;
+var startY = 200;
 // Add click event listener to each card
 cards.forEach(function (card, index) {
     var rotation = initialRotation + angle * index;
     var radians = rotation * (Math.PI / 180);
-    var x = Math.cos(radians) * radius;
-    var y = Math.sin(radians) * radius;
+    //const x = Math.cos(radians) * radius;
+    var y = 0;
+    var x = startX + (90 * index);
+    if (index < 5) {
+        var y = startY - (25 * index);
+    }
+    else {
+        var y = startY - 100 + (25 * (index - 4));
+    }
     console.log(x + ", " + y);
-    card.style.transform = "translate(calc(50% + ".concat(x, "px), calc(50% + ").concat(y, "px)) rotate(").concat(rotation, "deg)");
+    card.style.transform = "translate(calc(50% + ".concat(x, "px), calc(10% + ").concat(y, "px)) rotate(").concat(rotation, "deg)");
     card.addEventListener('click', function () {
         // Remove 'active' class from all cards
         cards.forEach(function (c) { return c.classList.remove('active'); });
--- deck-gen-main/src/main/resources/typescript/fanout.ts
+++ deck-gen-main/src/main/resources/typescript/fanout.ts
@@ -1,23 +1,35 @@
 // Get all the card elements
 const cards = document.querySelectorAll<HTMLElement>('.b-card');
 // Calculate the rotation angle for each card
-const angle = 20; // Adjust this value to change the fan-out angle
+const angle = 10; // Adjust this value to change the fan-out angle
 const totalCards = cards.length;
 const initialRotation = -angle * (totalCards - 1) / 2;
 console.log("hello");
 console.log(cards.length);
 // Calculate the radius of the arch
-const radius = 200; // Adjust this value to change the radius of the arch
-
+const radius = 400; // Adjust this value to change the radius of the arch
+const startX = 1;
+const startY = 200;
 // Add click event listener to each card
 cards.forEach((card, index) => {
   const rotation = initialRotation + angle * index;
   const radians = rotation * (Math.PI / 180);
-  const x = Math.cos(radians) * radius;
-  const y = Math.sin(radians) * radius;
+  //const x = Math.cos(radians) * radius;
+  var y = 0;
+  
+  const x = startX + (90*index);
+
+  if(index<5){
+    var y =  startY - (25*index);
+
+  }
+  else {
+    var y =  startY-100 + (25*(index-4));
+
+  }
   console.log(x + ", "+y);
 
-  card.style.transform = `translate(calc(50% + ${x}px), calc(50% + ${y}px)) rotate(${rotation}deg)`;
+  card.style.transform = `translate(calc(50% + ${x}px), calc(10% + ${y}px)) rotate(${rotation}deg)`;
 
   card.addEventListener('click', () => {
     // Remove 'active' class from all cards
