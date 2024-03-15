da66356eda25ae420921185e3b408ccff2f6858d
Keifer Wiseman <keiferwiseman@gmail.com>
2024-03-14 21:23:55 -0400

    keep resources folder

--- deckservice/src/main/java/com/kif/deckservice/DeckserviceApplication.java
+++ deckservice/src/main/java/com/kif/deckservice/DeckserviceApplication.java
@@ -2,6 +2,7 @@ package com.kif.deckservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -10,7 +11,7 @@ import com.kif.deckservice.config.AppProperties;
 @SpringBootApplication
 @ComponentScan({"com.kif.deckservice", "com.kif.deckgenmodels"})
 //@EnableConfigurationProperties(AppProperties.class)
-//@ConfigurationPropertiesScan("com.kif.deckservice.config")
+@ConfigurationPropertiesScan("com.kif.deckservice.config")
 public class DeckserviceApplication {
 
 	public static void main(String[] args) {
